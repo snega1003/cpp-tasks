2788f5ced167059cd9e69c15cfb1085e2c9dbf34
Snezhanna Roshchupkina <snega900@gmail.com>
2019-01-03 19:59:44 +0300

    task5

---
+++ task5/src/project/main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <numeric>
+#include "my_stream.hh"
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    istringstream str("0.1 0.2 0.3 0.4");
+    partial_sum(istream_iterator<double>(str),
+                      istream_iterator<double>(),
+                      my_stream_iterator<double, 3>(cout, " "));
+    return 0;
+}
