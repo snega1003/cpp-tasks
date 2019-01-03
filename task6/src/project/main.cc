bb752c55d350ec48393819da8e290f10bf2bfbfe
Snezhanna Roshchupkina <snega900@gmail.com>
2019-01-03 20:18:48 +0300

    task6

---
+++ task6/src/project/main.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <numeric>
+#include "man.hh"
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+int main()
+{
+	unordered_set<my::Man, size_t> days_off;
+	unordered_map<my::Man> salary;
+
+    return 0;
+}
