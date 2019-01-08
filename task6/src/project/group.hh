6c51b7bf987cbfbc722c715117e2b58241085fb8
Snezhanna Roshchupkina <snega900@gmail.com>
2019-01-08 18:09:44 +0300

    Update group.hh

--- task6/src/project/group.hh
+++ task6/src/project/group.hh
@@ -1,28 +1,29 @@
+#ifndef GROUP_HH
 #define GROUP_HH
 #include <unordered_set>
 #include <string>
 #include "man.hh"
 namespace my {
-    
-    using my::Man;
-    using std::string;
-    using std::unordered_set;
-    static int group_primary_key = 0;
 
-    class Group {
-        public:
-            void add(Man const & person)
-            {
-                database.insert(person);
-            }
-            Group(string const & n) : first_name(n), id(group_primary_key++) {};
-            
+	using my::Man;
+	using std::string;
+	using std::unordered_set;
+	static int group_key = 0;
 
-        private:
-            string group_name;
-            int id;
-            unordered_set<Man> database;
-    };
+	class Group {
+	public:
+		void add(Man const & person)
+		{
+			database.insert(person);
+		}
+		Group(string const & n) : first_name(n), id(group_key++) {};
+
+
+	private:
+		string group_name;
+		int id;
+		unordered_map<Man> database;
+	};
 
 }
 #endif
