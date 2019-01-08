57a8095299b1772e0ad1d12f1218a7e38b26dd52
Snezhanna Roshchupkina <snega900@gmail.com>
2019-01-08 23:48:54 +0300

    Update main.cc

--- task10/src/project/main.cc
+++ task10/src/project/main.cc
@@ -1,5 +1,14 @@
-#include <project/rotate.hh>
+#include "project/rotate.hh"
+#include <iostream>
+using namespace std;
 
 int main() {
-  return 0;
+	vector<double> vector({ 10, 100 });
+	int i;
+	cout << "i is number of rotations.\n i must be one of [1, 2, 3, 4, -1, -2, -3, 0, -4]\n so, your i is ";
+	cin >> i;
+	cout << endl;
+	my::rotate(vector, i);
+	cout << "rotated vector:" << vector[0] << " " << vector[1] << endl;
+
 }
