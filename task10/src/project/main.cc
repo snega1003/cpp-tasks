8cd79d19e25b380d84dbafa1ed8049dc8b16915a
Snezhanna Roshchupkina <snega900@gmail.com>
2019-01-08 21:58:50 +0300

    Create main.cc

---
+++ task10/src/project/main.cc
@@ -0,0 +1,23 @@
+#include "rotate_vector.h"
+
+void my::rotate(std::vector<double>& vect, int i) {
+	if (i % 4 == 1 || i % 4 == -3)
+	{
+		double a = vect[1];
+		vect[1] = -vect[0];
+		vect[0] = a;
+	}
+	else
+	if (i % 4 == 2 || i % 4 == -2)
+	{
+		vect[0] = -vect[0];
+		vect[1] = -vect[1];
+	}
+	else
+	if (i % 4 == 3 || i % 4 == -1)
+	{
+		double a = vect[0];
+		vect[0] = -vect[1];
+		vect[1] = a;
+	}
+}
