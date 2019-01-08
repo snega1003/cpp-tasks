fdab66f294b8399d2ad258aaca0221512979b613
Snezhanna Roshchupkina <snega900@gmail.com>
2019-01-08 18:11:47 +0300

    Update man.hh

--- task6/src/project/man.hh
+++ task6/src/project/man.hh
@@ -1,45 +1,44 @@
 #ifndef MAN_HH
 #define MAN_HH
-
 #include <string>
 
 namespace my {
-    using std::string;
-    
-    static int primary_key = 0;
+	using std::string;
 
-    class Man {
-        public:
-            Man(string const & n, string const & s) :
-                first_name(n), last_name(s), id(primary_key++) {};
-        
-        string to_string() const {
-            return last_name + " " + first_name;
-        }
+	static int primary_key = 0;
 
-        bool operator<(Man const & other) {
-            return to_string() < other.to_string();
-        }
-        int get_id() const {
-            return id;
-        }
-        private:
-            string first_name;
-            string last_name;
-            int id;
-    };
+	class Man {
+	public:
+		Man(string const & n, string const & s) :
+			first_name(n), last_name(s), id(primary_key++) {};
+
+		string to_string() const {
+			return last_name + " " + first_name;
+		}
+
+		bool operator<(Man const & other) {
+			return to_string() < other.to_string();
+		}
+		int get_id() const {
+			return id;
+		}
+	private:
+		string first_name;
+		string last_name;
+		int id;
+	};
 
 }
 
-template <> 
+template <>
 struct std::hash<my::Man>
 {
-    typedef size_t result_type;
-    typedef my::Man argument_type;
-    result_type operator()(const argument_type & x) const
-    {
-        return static_cast<result_type>(x.get_id());
-    }
+	typedef size_t result_type;
+	typedef my::Man argument_type;
+	result_type operator()(const argument_type & x) const
+	{
+		return static_cast<result_type>(x.get_id());
+	}
 
 };
 #endif
